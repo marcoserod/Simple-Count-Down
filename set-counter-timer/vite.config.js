9d5a05ed8f77e281cc592a9cdc381fe2a01d0435
Marcos Rodriguez <mer.marcosr@gamil.com>
2023-04-21 21:01:20 -0300

    Add manifest

--- set-counter-timer/vite.config.js
+++ set-counter-timer/vite.config.js
@@ -2,9 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
 
+const manifest = {
+    name: "Rest & Set Counter",
+    theme_color: "#000000",
+    background_color: "#FFFFFF",
+    display: "standalone",
+    scope: "/",
+    start_url: "/"
+  }
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),VitePWA()],
+  plugins: [react(),VitePWA(manifest)],
   server: {
     watch: {
       usePolling: true,
